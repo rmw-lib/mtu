2452816322444d03b81883b05808b511f1d44c59
gcxfd <i@rmw.link>
2022-07-13 16:36:29 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -19,26 +19,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let mtu_v4 = MtuV4::new();
 
   dbg!(mtu_v4.get(dest).await);
-  /*
-     let packet_slice = &mut [0; 1472];
-     let mut buf = vec![0; 8 + 1472]; // 8 bytes of header, then payload
-     let len = buf.len();
-     let mut packet = icmp::echo_request::MutableEchoRequestPacket::new(&mut buf[..]).unwrap();
-     packet.set_icmp_type(icmp::IcmpTypes::EchoRequest);
+  let packet_slice = &mut [0; 1472];
+  let mut buf = vec![0; 8 + 1472]; // 8 bytes of header, then payload
+  let len = buf.len();
+  let mut packet = icmp::echo_request::MutableEchoRequestPacket::new(&mut buf[..]).unwrap();
+  packet.set_icmp_type(icmp::IcmpTypes::EchoRequest);
 
   // Identifier为标识符，由主机设定，一般设置为进程号，回送响应消息与回送消息中identifier保持一致 && Sequence Number为序列号，由主机设定，一般设为由0递增的序列，回送响应消息与回送消息中Sequence Number保持一致
-  packet.set_identifier(2);
-  packet.set_sequence_number(len as u16 - 8);
+  //packet.set_identifier(2);
+  //packet.set_sequence_number(len as u16 - 8);
   packet.set_payload(packet_slice);
 
-  // Calculate and set the checksum
   let icmp_packet = icmp::IcmpPacket::new(packet.packet()).unwrap();
   let checksum = icmp::checksum(&icmp_packet);
   packet.set_checksum(checksum);
-  loop {
-  socket_clone.send_to(&mut packet.packet(), dest).unwrap();
-  std::thread::sleep(Duration::from_millis(1000));
-  }
-  */
+  let packet = packet.packet();
+
   Ok(())
 }
--- src/unix.rs
+++ src/unix.rs
@@ -1,7 +1,17 @@
-pub use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::{
+  net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4},
+  time::Duration,
+};
 
-pub use async_std::net::UdpSocket;
-use async_std::task::{spawn, JoinHandle};
+use async_std::{
+  future::{pending, timeout},
+  net::UdpSocket,
+  task::{spawn, JoinHandle},
+};
+use pnet_packet::{
+  icmp::{self},
+  Packet,
+};
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 pub fn v4(buf: &[u8]) -> u16 {
@@ -61,7 +71,26 @@ impl MtuV4 {
   }
 
   pub async fn get(&self, addr: SocketAddrV4) -> u16 {
-    //self.ing.insert(SocketAddrV4)
+    let mut buf = [0; 8 + 1472]; // 8 bytes of header, then payload
+    let len = buf.len();
+    let mut packet = icmp::echo_request::MutableEchoRequestPacket::new(&mut buf[..]).unwrap();
+    packet.set_icmp_type(icmp::IcmpTypes::EchoRequest);
+
+    // Identifier为标识符，由主机设定，一般设置为进程号，回送响应消息与回送消息中identifier保持一致 && Sequence Number为序列号，由主机设定，一般设为由0递增的序列，回送响应消息与回送消息中Sequence Number保持一致
+    //packet.set_identifier(2);
+    //packet.set_sequence_number(len as u16 - 8);
+    packet.set_payload(&[0; 1472]);
+
+    let icmp_packet = icmp::IcmpPacket::new(packet.packet()).unwrap();
+    let checksum = icmp::checksum(&icmp_packet);
+    packet.set_checksum(checksum);
+
+    err::log!(self.udp.send_to(packet.packet(), addr));
+
+    let never = pending::<()>();
+    let dur = Duration::from_secs(5);
+
+    err::log!(timeout(dur, never).await);
     0
   }
 }
