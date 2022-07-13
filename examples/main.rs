58d3371159e4b6a8877d7ca61aaa37f86cea250a
gcxfd <i@rmw.link>
2022-07-13 13:44:51 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -45,6 +45,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let len = buf.len();
     let mut packet = icmp::echo_request::MutableEchoRequestPacket::new(&mut buf[..]).unwrap();
     packet.set_icmp_type(icmp::IcmpTypes::EchoRequest);
+
+    // Identifier为标识符，由主机设定，一般设置为进程号，回送响应消息与回送消息中identifier保持一致 && Sequence Number为序列号，由主机设定，一般设为由0递增的序列，回送响应消息与回送消息中Sequence Number保持一致
     packet.set_identifier(1);
     packet.set_sequence_number(len as u16 - 8);
     packet.set_payload(packet_slice);
